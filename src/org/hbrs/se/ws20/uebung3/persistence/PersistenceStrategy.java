dd617794d81d54110652985bddf726ed064efaae
saschaalda <sascha.alda@h-brs.de>
2020-11-17 19:16:23 +0100

    Weitere Kommentare hinzugefuegt zur Erläuterung des Strategy Pattern sowie zur Implementierung eines Streams. Zudem wurde die Exception weiter erklärt

--- src/org/hbrs/se/ws20/uebung3/persistence/PersistenceStrategy.java
+++ src/org/hbrs/se/ws20/uebung3/persistence/PersistenceStrategy.java
@@ -2,6 +2,14 @@ package org.hbrs.se.ws20.uebung3.persistence;
 
 import java.util.List;
 
+/**
+ * Interface for defining basic methods for a persistence mechanism
+ * Each concrete algorithm (i.e. strategy) must implement this method
+ * This interface corresponds to the abstract strategy w.r.t. to the
+ * Strategy Design Pattern (GoF).
+ *
+ * @param <E>
+ */
 public interface PersistenceStrategy<E> {
     public void openConnection() throws PersistenceException;
     public void closeConnection() throws PersistenceException;
--- src/org/hbrs/se/ws20/uebung3/persistence/PersistenceStrategyStream.java
+++ src/org/hbrs/se/ws20/uebung3/persistence/PersistenceStrategyStream.java
@@ -1,5 +1,7 @@
 package org.hbrs.se.ws20.uebung3.persistence;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.util.List;
 
 public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Member> {
@@ -14,12 +16,36 @@ public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Me
     }
 
     @Override
+    /**
+     * Method for saving a list of Member-objects to a disk (HDD)
+     */
     public void save(List<Member> member) throws PersistenceException  {
 
     }
 
     @Override
+    /**
+     * Method for loading a list of Member-objects from a disk (HDD)
+     * Some coding examples come for free :-)
+     */
     public List<Member> load() throws PersistenceException  {
+        // Some Coding hints ;-)
+        // ObjectInputStream ois = null;
+        // FileInputStream fis = null;
+        // List<...> newListe =  null;
+        //
+        // Initiating the Stream (can also be moved to method openConnection()... ;-)
+        // fis = new FileInputStream( " a location to a file" );
+        // ois = new ObjectInputStream(fis);
+
+        // Reading and extracting the list (try .. catch ommitted here)
+        // Object obj = ois.readObject();
+
+        // if (obj instanceof List<?>) {
+        //       newListe = (List) obj;
+        // return newListe
+
+        // and finally close the streams (guess where this could be...?)
         return null;
     }
 }
