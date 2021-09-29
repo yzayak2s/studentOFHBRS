56304e06570b1f5bf13873b4807310da5f54301c
saschaalda <sascha.alda@h-brs.de>
2021-09-29 08:18:19 +0200

    Delete

--- src/org/hbrs/se/ws20/prototype/uebung4/Command.java
+++
@@ -1,7 +0,0 @@
-package org.hbrs.se.ws20.prototype.uebung4;
-
-public interface Command {
-
-    public void execute( String[] args );
-    public void undo();
-}
--- src/org/hbrs/se/ws20/uebung3/Info.java
+++
@@ -1,8 +0,0 @@
-package org.hbrs.se.ws20.uebung3;
-
-public interface Info {
-	
-	public String getName();
-	public void setName(String name);
-
-}  
