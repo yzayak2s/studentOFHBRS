2f7803a9deaca0cc731adddf237b7e10e0fa23e5
saschaalda <sascha.alda@h-brs.de>
2021-10-04 12:25:18 +0200

    Upload für Übung Nr. 1

---
+++ src/org/hbrs/se1/ws21/uebung1/view/Client.java
@@ -0,0 +1,23 @@
+package org.hbrs.se1.ws21.uebung1.view;
+
+public class Client {
+
+	/*
+	 * Methode zur Ausgabe einer Zahl auf der Console
+	 */
+	public void display( int aNumber ){
+		// In dieser Methode soll die Methode translateNumber 
+		// mit dem übergegebenen Wert der Variable aNumber 
+		// aufgerufen werden.
+		//
+		// Strenge Implementierung gegen das Interface Translator gewuenscht!
+
+		System.out.println("Das Ergebnis der Berechnung: " +
+				"[das Ergebnis an dieser Stelle]"  );
+
+	}
+}
+
+
+
+
