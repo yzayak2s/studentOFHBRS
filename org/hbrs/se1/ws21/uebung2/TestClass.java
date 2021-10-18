437725fe30fa0832fecdf5a99caaeae076339f38
= <=>
2021-10-19 01:41:26 +0200

    Fix Test

--- org/hbrs/se1/ws21/uebung2/TestClass.java
+++ org/hbrs/se1/ws21/uebung2/TestClass.java
@@ -6,9 +6,9 @@ public class TestClass {
     @Test
     public void test(){
         Container container = new Container();
-        Member member1 = container.addMember(Container.newMember().setID(33));
-        Member member2 = container.addMember(Container.newMember().setID(0));
-        Member member3 = container.addMember(Container.newMember().setID(-1));
+        Member member1 = container.addMember(Container.newMember()).setID(33);
+        Member member2 = container.addMember(Container.newMember()).setID(0);
+        Member member3 = container.addMember(Container.newMember()).setID(-1);
         assertEquals("33", member1.toString());
         assertEquals("0", member2.toString());
         assertEquals("-1", member3.toString());
