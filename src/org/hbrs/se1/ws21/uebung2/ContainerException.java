8d84282466d010bf7186e93b545690c92b8e981f
= <=>
2021-10-19 01:35:49 +0200

    Testing & Fix Directories

--- src/org/hbrs/se1/ws21/uebung2/ContainerException.java
+++
@@ -1,7 +0,0 @@
-package src.org.hbrs.se1.ws21.uebung2;
-
-public class ContainerException extends Exception {
-    public ContainerException(Member member){
-        super("Das Member-Objekt mit der ID " + member.getID() + " ist bereits vorhanden!");
-    }
-}
