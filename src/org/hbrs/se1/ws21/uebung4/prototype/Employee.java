72050a74aec04c83c6d84c1a063e4989b55eff50
saschaalda <sascha.alda@h-brs.de>
2021-11-03 08:17:10 +0100

    Erste Lösung von Julius P. (noch ohne Korrekturen aus der Übung)

---
+++ src/org/hbrs/se1/ws21/uebung4/prototype/Employee.java
@@ -0,0 +1,43 @@
+package org.hbrs.se1.ws21.uebung4.prototype;
+
+public class Employee {
+
+    private String vorname;
+    private String name;
+    private Integer pid;
+    private String abteilung;
+
+    public String getAbteilung() {
+        return abteilung;
+    }
+
+    public void setAbteilung(String abteilung) {
+        this.abteilung = abteilung;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+}
+
