2276a34bae21f918a529aa9a706247594360d122
saschaalda <sascha.alda@h-brs.de>
2021-12-15 18:18:24 +0100

    Upload der Musterlösung der Übung Nr. 8-1

---
+++ src/org/hbrs/se1/ws21/solutions/uebung8/portal/ExternalHotelSucheInterface.java
@@ -0,0 +1,7 @@
+package org.hbrs.se1.ws21.solutions.uebung8.portal;
+
+public interface ExternalHotelSucheInterface {
+	
+	public SuchErgebnis suche(SuchAuftrag auftrag);
+
+}
---
+++ src/org/hbrs/se1/ws21/solutions/uebung8/reiseanbieter/QueryResult.java
@@ -0,0 +1,5 @@
+package org.hbrs.se1.ws21.solutions.uebung8.reiseanbieter;
+
+public class QueryResult {
+
+}
